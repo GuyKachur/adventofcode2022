9339f7c935d688d9c921cfdbfa83c475171b791a
def <gkachur@gmail.com>
2022-12-09 21:51:29 -0800

    day8-part2

--- day8/day8.go
+++ day8/day8.go
@@ -33,10 +33,6 @@ func Run() {
 		fmt.Println(err)
 	}
 
-	//now that the grid is created lets find all the trees that are accesible
-	//left
-	// top, left, right, bottom := 0, 0, 0, 0
-	canSee := make(map[string]bool, 0)
 	results := make([][]int, len(grid))
 	for i := range results {
 		results[i] = make([]int, len(grid[i]))
@@ -46,77 +42,68 @@ func Run() {
 	//LEFT
 	//for each row
 	for i := 0; i < len(grid); i++ {
-		leftMax := -1
-		// go through each col an
 		for j := 0; j < len(grid[i]); j++ {
-			//if each cell can see the top mark as seen
-			// update the max heigh weve seen
-			if grid[i][j] > leftMax {
-				leftMax = grid[i][j]
-				canSee[fmt.Sprintf("%d:%d", i, j)] = true
-				results[i][j] = 1
+			results[i][j] = scenicScore(grid, i, j)
+		}
+	}
+
+	max := 0
+	for _, row := range results {
+		for _, col := range row {
+			if col > max {
+				max = col
 			}
 		}
 	}
 
-	//TOP
-	//for every col
-	for j := 0; j < len(grid[0]); j++ {
-		topMax := -1
-		// go through every row
-		for i := 0; i < len(grid); i++ {
-			if grid[i][j] > topMax {
-				topMax = grid[i][j]
-				canSee[fmt.Sprintf("%d:%d", i, j)] = true
-				results[i][j] = 1
-			}
+	fmt.Println(fmt.Sprintf("\nDay %d: %d\n", num, max))
+
+}
+
+// next steps would be memoization
+// really this is a dp problem, you can probably do the same solve from top left and bottom right again, each time calcuating the score for the tree? idk
+func scenicScore(grid [][]int, row, col int) int {
+	//scenic score is found by multiplying each of the directions number of trees scene...
+	//look up
+	up := grid[row][col]
+	upSeen := 0
+	for i := col - 1; i >= 0; i-- {
+		upSeen++
+		if grid[row][i] >= up {
+			break
 		}
 	}
 
-	//RIGHT
-	//for each row
-	for i := len(grid) - 1; i >= 0; i-- {
-		rightMax := -1
-		// go through each col an
-		for j := len(grid[0]) - 1; j >= 0; j-- {
-			//if each cell can see the top mark as seen
-			// update the max heigh weve seen
-			if grid[i][j] > rightMax {
-				rightMax = grid[i][j]
-				canSee[fmt.Sprintf("%d:%d", i, j)] = true
-				results[i][j] = 1
-			}
+	//look right
+	right := grid[row][col]
+	rightSeen := 0
+	for i := row + 1; i < len(grid); i++ {
+		rightSeen++
+		if grid[i][col] >= right {
+			break
 		}
 	}
 
-	//BOTTOM
-	//for every col
-	for j := len(grid[0]) - 1; j >= 0; j-- {
-		bottomMax := -1
-		// go through every row
-		for i := len(grid) - 1; i >= 0; i-- {
-			if grid[i][j] > bottomMax {
-				bottomMax = grid[i][j]
-				canSee[fmt.Sprintf("%d:%d", i, j)] = true
-				results[i][j] = 1
-			}
+	//look down
+	down := grid[row][col]
+	downSeen := 0
+	for i := col + 1; i < len(grid); i++ {
+		downSeen++
+		if grid[row][i] >= down {
+			break
 		}
 	}
 
-	//if i is row
-	// and j is col
-	// then as i go down a col i should keep track of top max
-	// and as i go down a row keep track of leftmax
-
-	total := 0
-	for k, v := range canSee {
-		if v {
-			total++
-		} else {
-			fmt.Println(k)
+	//look left
+	left := grid[row][col]
+	leftSeen := 0
+	for i := row - 1; i >= 0; i-- {
+		leftSeen++
+		if grid[i][col] >= left {
+			break
 		}
 	}
 
-	fmt.Println(fmt.Sprintf("\nDay %d: %d\n", num, total))
+	return upSeen * rightSeen * downSeen * leftSeen
 
 }
